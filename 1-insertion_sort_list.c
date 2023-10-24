d7864b7a45a70fd9579070c7beec1a61893f7a26
Chukwunonso10 <kuzuechinonsojude@gmail.com>
2023-10-24 14:37:05 -0700

    isertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a Doubly linked list
+ * ascending order using the insertion sort algorithm
+ * @list: doubly linked list
+ *
+ * Return: no return
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *p, *tmp;
+
+	if (!list || !(*list)->next)
+		return;
+
+	p = *list;
+	while (p)
+	{
+		while (p->next && (p->n > p->next->n))
+		{
+			tmp = p->next;
+			p->next = tmp->next;
+			tmp->prev = p->prev;
+
+			if (p->prev)
+				p->prev->next = tmp;
+			if (tmp->next)
+				tmp->next->prev = p;
+			p->prev = tmp;
+			tmp->next = p;
+
+			if (tmp->prev)
+				p = tmp->prev;
+			else
+				*list = tmp;
+			print_list(*list);
+		}
+		p = p->next;
+	}
+}
